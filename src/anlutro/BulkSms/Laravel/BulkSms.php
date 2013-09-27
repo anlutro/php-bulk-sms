71a31e261509f33d8791c32b1080456586f90303
Andreas Lutro <anlutro@gmail.com>
2013-09-27 08:31:45 +0200

    I misunderstood facades!

--- src/anlutro/BulkSms/Laravel/BulkSms.php
+++ src/anlutro/BulkSms/Laravel/BulkSms.php
@@ -25,21 +25,4 @@ class BulkSms extends Facade
 	{
 		return 'bulksms';
 	}
-
-	/**
-	 * Send a view with data to a recipient. Made to imitate Laravel's
-	 * Mail::send syntax.
-	 *
-	 * @param  string $view
-	 * @param  array  $data
-	 * @param  string $recipient Phone number
-	 *
-	 * @return void
-	 */
-	public static function send($view, $data, $recipient)
-	{
-		$message = \Illuminate\Support\Facades\View::make($view, $data)
-			->render();
-		return static::sendMessage($recipient, $message);
-	}
 }
