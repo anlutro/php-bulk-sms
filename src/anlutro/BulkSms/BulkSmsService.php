2cb209db3df9d371e15a51a3cc6629dc0f753bb7
Michael Nowag <michaelnowag@gmail.com>
2014-11-12 17:05:05 +0100

    added support for custom single and bulk urls

--- src/anlutro/BulkSms/BulkSmsService.php
+++ src/anlutro/BulkSms/BulkSmsService.php
@@ -17,50 +17,65 @@ use anlutro\cURL\cURL;
 class BulkSmsService
 {
 	/**
-	 * BulkSMS username
-	 *
-	 * @var string
+     * Meaning of response status codes.
+     *
+     * @var array
 	 */
-	protected $username;
-
+    protected static $statusMessages = array(
+        0   => 'In progress',
+        1   => 'Scheduled',
+        22  => 'Internal fatal error',
+        23  => 'Authentication error',
+        24  => 'Data validation failed',
+        25  => 'Insufficient credits',
+        26  => 'Upstream credits not available',
+        27  => 'Daily quota exceeded',
+        28  => 'Upstream quota exceeded',
+        40  => 'Temporarily unavailable',
+        201 => 'Maximum batch size exceeded',
+    );
 	/**
-	 * BulkSMS password
+     * BulkSMS username
+
 	 *
-	 * @var string
-	 */
-	protected $password;
-
+*@var string
+     */
+    protected $username;
 	/**
-	 * Meaning of response status codes.
-	 *
-	 * @var array
-	 */
-	protected static $statusMessages = array(
-		0 => 'In progress',
-		1 => 'Scheduled',
-		22 => 'Internal fatal error',
-		23 => 'Authentication error',
-		24 => 'Data validation failed',
-		25 => 'Insufficient credits',
-		26 => 'Upstream credits not available',
-		27 => 'Daily quota exceeded',
-		28 => 'Upstream quota exceeded',
-		40 => 'Temporarily unavailable',
-		201 => 'Maximum batch size exceeded',
-	);
+     * BulkSMS password
+     *
+     * @var string
+     */
+    protected $password;
+    /**
+     * @var null
+     */
+    protected $bulkurl;
+    /**
+     * @var null
+     */
+    protected $singleurl;
 
-	/**
+    /**
 	 * @param string $username BulkSMS username
 	 * @param string $password BulkSMS password
 	 * @param anlutro\cURL\cURL $curl  (optional) If you have an existing
 	 *   instance of my cURL wrapper, you can pass it.
-	 */
-	public function __construct($username, $password, $curl = null)
+     */
+    public function __construct($username, $password, $curl = null, $singleurl = null, $bulkurl = null)
 	{
 		$this->username = $username;
 		$this->password = $password;
 		$this->curl = $curl ?: new cURL;
-	}
+
+        if ($bulkurl) {
+            $this->bulkurl = $bulkurl;
+        }
+        if ($singleurl) {
+            $this->singleurl = $singleurl;
+        }
+
+    }
 
 	/**
 	 * Send a single message.
@@ -81,91 +96,93 @@ class BulkSmsService
 		return $this->parseResponse($sender->send());
 	}
 
-	/**
-	 * Send messages in bulk.
-	 *
-	 * @param  array  $messages associative array of recipient => message
-	 *
-	 * @return mixed
-	 */
-	public function sendBulkMessages(array $messages)
-	{
-		$sender = $this->createBulkSender();
-
-		foreach ($messages as $recipient => $message) {
-			$msg = $this->createMessage($recipient, $message);
-			$sender->addMessage($msg);
-		}
-
-		return $this->parseResponse($sender->send());
+    /**
+     * Create a message sender instance.
+     *
+     * @return anlutro\BulkSms\Sender\Single
+     */
+    protected function createMessageSender()
+    {
+        return new Sender\Single($this->username, $this->password, $this->curl, $this->singleurl);
 	}
 
-	/**
-	 * Parse a response from the API.
-	 *
-	 * @param  anlutro\cURL\Response $response
-	 *
-	 * @return boolean
-	 */
-	public function parseResponse($response)
-	{
-		if ($response->code !== '200 OK') {
-			throw new BulkSmsException('BulkSMS API responded with HTTP status code ' . $response->code);
-		}
+    /**
+     * Create a message instance.
+     *
+     * @param  string $recipient
+     * @param  string $message
+     *
+     * @return anlutro\BulkSms\Message
+     */
+    protected function createMessage($recipient, $message)
+    {
+        $msg = new Message;
 
-		$parts = explode('|', $response->body);
+        $msg->recipient($recipient);
+        $msg->message($message);
 
-		if (!is_numeric($parts[0])) {
-			throw new \UnexpectedValueException('Unknown response code: ' . $parts[0] . ' - full response: ' . $response->body);
-		}
+        return $msg;
+    }
 
-		$code = (int) $parts[0];
+    /**
+     * Parse a response from the API.
+     *
+     * @param  anlutro\cURL\Response $response
+     *
+     * @return boolean
+     */
+    public function parseResponse($response)
+    {
+        if ($response->code !== '200 OK') {
+            throw new BulkSmsException('BulkSMS API responded with HTTP status code ' . $response->code);
+        }
 
-		if ($code === 0 || $code === 1) {
-			return true;
-		} else {
-			$message = array_key_exists($code, static::$statusMessages)
-				? static::$statusMessages[$code]
-				: $parts[1];
-			throw new BulkSmsException('BulkSMS API responded with code: ' . $code . ' - ' . $message);
-		}
-	}
+        $parts = explode('|', $response->body);
 
-	/**
-	 * Create a message instance.
-	 *
-	 * @param  string $recipient
-	 * @param  string $message
-	 *
-	 * @return anlutro\BulkSms\Message
-	 */
-	protected function createMessage($recipient, $message)
-	{
-		$msg = new Message;
-		
-		$msg->recipient($recipient);
-		$msg->message($message);
-		
-		return $msg;
-	}
+        if (!is_numeric($parts[ 0 ])) {
+            throw new \UnexpectedValueException(
+                'Unknown response code: ' . $parts[ 0 ] . ' - full response: ' . $response->body
+            );
+        }
 
-	/**
-	 * Create a message sender instance.
-	 *
-	 * @return anlutro\BulkSms\Sender\Single
-	 */
-	protected function createMessageSender()
-	{
-		return new Sender\Single($this->username, $this->password, $this->curl);
+        $code = (int) $parts[ 0 ];
+
+        if ($code === 0 || $code === 1) {
+            return true;
+        } else {
+            $message = array_key_exists($code, static::$statusMessages)
+                ? static::$statusMessages[ $code ]
+                : $parts[ 1 ];
+            throw new BulkSmsException('BulkSMS API responded with code: ' . $code . ' - ' . $message);
+        }
+    }
+
+    /**
+     * Send messages in bulk.
+     *
+     * @param  array $messages associative array of recipient => message
+     *
+     * @return mixed
+     */
+    public function sendBulkMessages(array $messages)
+    {
+        $sender = $this->createBulkSender();
+
+        foreach ($messages as $recipient => $message) {
+            $msg = $this->createMessage($recipient, $message);
+            $sender->addMessage($msg);
+        }
+
+        return $this->parseResponse($sender->send());
 	}
 
 	/**
 	 * Create a message sender instance.
 	 *
 	 * @return anlutro\BulkSms\Sender\Bulk
-	 */
-	protected function createBulkSender()
-	{
-		return new Sender\Bulk($this->username, $this->password, $this->curl);
+     */
+    protected function createBulkSender()
+    {
+        return new Sender\Bulk($this->username, $this->password, $this->curl, $this->bulkurl);
 	}
 }
--- tests/BulkSmsServiceTest.php
+++ tests/BulkSmsServiceTest.php
@@ -24,6 +24,16 @@ class BulkSmsServiceTest extends PHPUnit_Framework_TestCase
 		$this->assertTrue($bsms->sendMessage('12345678', 'hello'));
 	}
 
+    public function mockCurl()
+    {
+        return m::mock('anlutro\cURL\cURL');
+    }
+
+    public function makeService($username, $password, $curl = null, $singleurl = null, $bulkurl = null)
+    {
+        return new anlutro\BulkSms\BulkSmsService($username, $password, $curl, $singleurl, $bulkurl);
+    }
+
 	/**
 	 * @expectedException anlutro\BulkSms\BulkSmsException
 	 */
@@ -79,13 +89,24 @@ class BulkSmsServiceTest extends PHPUnit_Framework_TestCase
 		$this->assertTrue($bsms->sendMessage('12345678', $message));
 	}
 
-	public function makeService($username, $password, $curl = null)
-	{
-		return new anlutro\BulkSms\BulkSmsService($username, $password, $curl);
-	}
+    public function testSendSingleConcatCustomURL()
+    {
+        $message            = str_repeat('x', 200);
+        $expectedPostData   = array(
+            'username'                  => 'foo',
+            'password'                  => 'bar',
+            'message'                   => $message,
+            'msisdn'                    => '4712345678',
+            'allow_concat_text_sms'     => 1,
+            'concat_text_sms_max_parts' => 2,
+        );
+        $mockResponse       = new StdClass;
+        $mockResponse->code = '200 OK';
+        $mockResponse->body = '0|IN_PROGRESS|12345678';
+        $curl               = $this->mockCurl();
+        $curl->shouldReceive('post')->once()->with('http://bulksms.de', $expectedPostData)->andReturn($mockResponse);
+        $bsms = $this->makeService('foo', 'bar', $curl, "http://bulksms.de");
 
-	public function mockCurl()
-	{
-		return m::mock('anlutro\cURL\cURL');
-	}
+        $this->assertTrue($bsms->sendMessage('12345678', $message));
+    }
 }
